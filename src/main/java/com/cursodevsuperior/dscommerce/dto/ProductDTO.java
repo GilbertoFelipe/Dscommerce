b274ed88c75b82c870549b8d56f37d4d4c943ff9
Gilberto Felipe <gilbertofelipe.junior@gmail.com>
2024-10-08 16:55:47 -0300

    ProductDTO with categories

--- src/main/java/com/cursodevsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/cursodevsuperior/dscommerce/dto/ProductDTO.java
@@ -1,9 +1,14 @@
 package com.cursodevsuperior.dscommerce.dto;
 
+import com.cursodevsuperior.dscommerce.entities.Category;
 import com.cursodevsuperior.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDTO {
 
@@ -21,6 +26,8 @@ public class ProductDTO {
     private Double price;
     private String imgUrl;
 
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -36,6 +43,9 @@ public class ProductDTO {
         description = entity.getDescription();
         price = entity.getPrice();
         imgUrl = entity.getImgUrl();
+        for (Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat));
+        }
     }
 
 
@@ -57,5 +67,9 @@ public class ProductDTO {
 
     public String getImgUrl() {
         return imgUrl;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
 }
--- src/main/java/com/cursodevsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/cursodevsuperior/dscommerce/services/ProductService.java
@@ -1,7 +1,9 @@
 package com.cursodevsuperior.dscommerce.services;
 
+import com.cursodevsuperior.dscommerce.dto.CategoryDTO;
 import com.cursodevsuperior.dscommerce.dto.ProductDTO;
 import com.cursodevsuperior.dscommerce.dto.ProductMinDTO;
+import com.cursodevsuperior.dscommerce.entities.Category;
 import com.cursodevsuperior.dscommerce.entities.Product;
 import com.cursodevsuperior.dscommerce.repositories.ProductRepository;
 import com.cursodevsuperior.dscommerce.services.exceptions.DatabaseException;
@@ -77,6 +79,13 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category cat = new Category();
+            cat.setId(catDto.getId());
+            entity.getCategories().add(cat);
+        }
     }
 
 }
